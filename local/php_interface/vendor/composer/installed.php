2ff16baece55d5b5878951022a5ab9286d0c51b0
Suntechnic <madzhugin@gmail.com>
2022-05-04 11:22:01 +0000

    full:alpha

---
+++ local/php_interface/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2953d578b5ea869a29ffe84cc37ea1360bdc3985',
+        'name' => 'app/x',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'app/x' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2953d578b5ea869a29ffe84cc37ea1360bdc3985',
+            'dev_requirement' => false,
+        ),
+        'kint-php/kint' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kint-php/kint',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'fa53c4333cda79dc9cb002cfa029abe994d0ae00',
+            'dev_requirement' => true,
+        ),
+    ),
+);
