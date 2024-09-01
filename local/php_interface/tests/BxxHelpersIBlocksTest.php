0b5b7952e30a806f3bbda5975cc5348fd1499c88
Александр Маджугин <madzhugin@gmail.com>
2024-09-01 20:31:25 +0000

    добавление unit-тестов

---
+++ local/php_interface/tests/BxxHelpersIBlocksTest.php
@@ -0,0 +1,51 @@
+<?php
+use PHPUnit\Framework\Attributes\DataProvider;
+
+/**
+ * Class BxxHelpersIBlocksTest
+ */
+class BxxHelpersIBlocksTest extends \PHPUnit\Framework\TestCase
+{
+    
+    // private $metaphour;
+    
+    // public function __construct ()
+    // {
+        
+    //     return parent::__construct();
+    // }
+
+
+
+
+    public static function lstIBlocksProvider(): array
+    {
+        \Bxx\Core::init(['iblock']);
+
+        $rdb = \CIBlock::GetList(
+                [],
+                ['!CODE' => [false,'']]
+            );
+
+        $lstIBlocks = [];
+        while ($dctIBlock = $rdb->fetch()) {
+            $lstIBlocks[] = 
+                [intval($dctIBlock['ID']),$dctIBlock['CODE']];
+            if (count($lstIBlocks)>20) break;
+        }
+
+        return $lstIBlocks;
+    }
+    
+    // тестирование получения id инфоблоков по коду ИБ
+    #[DataProvider('lstIBlocksProvider')]
+    public function testGetIdByCode(int $Id, string $Code): void
+    {
+        $this->assertSame(
+                $Id,
+                \Bxx\Helpers\IBlocks::getIdByCode($Code)
+            );
+    }
+
+
+}
