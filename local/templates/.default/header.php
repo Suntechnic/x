f6c72f35bbc230662ee007ec50d186a34adb784e
Suntechnic <madzhugin@yandex.ru>
2024-11-05 23:12:16 +0300

    Блокировка страниц паблика открываемых в обход роутера

--- local/templates/.default/header.php
+++ local/templates/.default/header.php
@@ -1,4 +1,9 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$bxApp = \Bitrix\Main\Application::getInstance();
+$router = $bxApp->getRouter();
+$request = $bxApp->getContext()->getRequest();
+if (!$router->match($request)) die();    
+?>
 <!DOCTYPE html>
 <html lang="<?=LANGUAGE_ID?>">
 <head>
