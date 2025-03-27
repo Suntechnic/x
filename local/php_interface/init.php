d695351d85cd004cf8008dfd133da6f8deae6262
Александр Маджугин <madzhugin@gmail.com>
2025-03-27 09:59:59 +0000

    bootstrap и опции приложения

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,26 +1,4 @@
 <?php
-// установите на тестовй среде:
-// SetEnv APPLICATION_ENV 'dev'
-
-if ($_SERVER['APPLICATION_ENV'] || $_SERVER['REDIRECT_APPLICATION_ENV']) {
-    if (!$_SERVER['APPLICATION_ENV']) $_SERVER['APPLICATION_ENV'] = $_SERVER['REDIRECT_APPLICATION_ENV'];
-    define('APPLICATION_ENV',$_SERVER['APPLICATION_ENV']);
-} else {
-    define('APPLICATION_ENV','production');
-}
-
-require __DIR__ . '/vendor/autoload.php';
-
-\Kint\Renderer\RichRenderer::$folder = true;
-if (!defined('APPLICATION_ENV') || APPLICATION_ENV != 'dev') {
-    \Kint::$enabled_mode = false;
-} else if (defined('APPLICATION_ENV') || APPLICATION_ENV != 'production') {
-    define('VUEJS_DEBUG', true);
-}
-
-$DefaultTemplatePath = \Bitrix\Main\Application::getDocumentRoot().'/local/templates/.default';
-define('DEFAULT_TEMPLATE_PATH',$DefaultTemplatePath);
-
 // подгрузка всего из папки init
 $lstInitsFile = scandir(__DIR__.'/init');
 if ($lstInitsFile) $lstInitsFile = array_filter($lstInitsFile,function ($N) {return (
---
+++ local/php_interface/init/.bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+// установите на тестовй среде:
+// SetEnv APPLICATION_ENV 'dev'
+
+// определение констаны типа окружения
+if ($_SERVER['APPLICATION_ENV'] || $_SERVER['REDIRECT_APPLICATION_ENV']) {
+    if (!$_SERVER['APPLICATION_ENV']) $_SERVER['APPLICATION_ENV'] = $_SERVER['REDIRECT_APPLICATION_ENV'];
+    define('APPLICATION_ENV',$_SERVER['APPLICATION_ENV']);
+} else {
+    define('APPLICATION_ENV','production');
+}
+
+// автозагрузка классов
+include_once(__DIR__.'/../vendor/autoload.php');
+
+// подключение и преднастройка библиотеки Kint
+if (class_exists('\Kint')) { 
+    \Kint\Renderer\RichRenderer::$folder = true;
+    if (!defined('APPLICATION_ENV') || APPLICATION_ENV != 'dev') {
+        \Kint::$enabled_mode = false;
+    } else if (defined('APPLICATION_ENV') || APPLICATION_ENV != 'production') {
+        define('VUEJS_DEBUG', true);
+    }
+}
+
+// установка пути к шаблону по умолчанию
+$DefaultTemplatePath = \Bitrix\Main\Application::getDocumentRoot().'/local/templates/.default';
+define('DEFAULT_TEMPLATE_PATH',$DefaultTemplatePath);
