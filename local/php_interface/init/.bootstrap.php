61fd06b8f9f99b43af7b5c9cb6f72a9e3fde162d
Александр Маджугин <madzhugin@gmail.com>
2025-08-05 20:04:32 +0000

    предупреждение о режиме разработки

--- local/php_interface/init/.bootstrap.php
+++ local/php_interface/init/.bootstrap.php
@@ -1,9 +1,20 @@
 <?php
-// установите на тестовй среде:
-// SetEnv APPLICATION_ENV 'dev'
+/**
+ * Файл устанавливает две константы:
+ * APPLICATION_ENV - тип окружения (production, dev)
+ * DEFAULT_TEMPLATE_PATH - путь к шаблону по умолчанию (прибит гвоздями к .default)
+ * Также подключает автозагрузчик классов
+ * и отключает отладку Kint в режиме production.
+ * 
+ * установите на тестовой среде APPLICATION_ENV 'dev'
+ * и на продакшн APPLICATION_ENV 'production'
+ * 
+ * Пример для htaccess:
+ * SetEnv APPLICATION_ENV 'dev'
+ */
 
 // определение констаны типа окружения
-if (isset($_SERVER['APPLICATION_ENV']) || isset($_SERVER['REDIRECT_APPLICATION_ENV'])) {
+if ($_SERVER['APPLICATION_ENV'] || $_SERVER['REDIRECT_APPLICATION_ENV']) {
     if (!$_SERVER['APPLICATION_ENV']) $_SERVER['APPLICATION_ENV'] = $_SERVER['REDIRECT_APPLICATION_ENV'];
     define('APPLICATION_ENV',$_SERVER['APPLICATION_ENV']);
 } else {
@@ -13,15 +24,18 @@ if (isset($_SERVER['APPLICATION_ENV']) || isset($_SERVER['REDIRECT_APPLICATION_E
 // автозагрузка классов
 include_once(__DIR__.'/../vendor/autoload.php');
 
-// подключение и преднастройка библиотеки Kint
-if (class_exists('\Kint')) { 
-    \Kint\Renderer\RichRenderer::$folder = true;
-    if (!defined('APPLICATION_ENV') || APPLICATION_ENV != 'dev') {
-        \Kint::$enabled_mode = false;
-    } else if (defined('APPLICATION_ENV') || APPLICATION_ENV != 'production') {
-        define('VUEJS_DEBUG', true);
-    }
+if (!defined('APPLICATION_ENV') || APPLICATION_ENV != 'dev') { // если не дев
+    if (class_exists('\Kint')) \Kint::$enabled_mode = false; // погасим отдалку если она есть
+} else if (defined('APPLICATION_ENV') || APPLICATION_ENV != 'production') { // если не продакшн...
+    define('VUEJS_DEBUG', true);
+    if (class_exists('\Kint')) \Kint\Renderer\RichRenderer::$folder = true;
+    \CAdminNotify::Add([
+            'MESSAGE' => 'Сайт работает в режиме разработки',
+            'TAG' => 'DEVELOPMENT_MODE',
+            'NOTIFY_TYPE' => \CAdminNotify::TYPE_ERROR
+        ]);
 }
+
 
 // установка пути к шаблону по умолчанию
 $DefaultTemplatePath = \Bitrix\Main\Application::getDocumentRoot().'/local/templates/.default';
