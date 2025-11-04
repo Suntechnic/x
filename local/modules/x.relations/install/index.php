5ca1a863736ef15481f57310a8e1f54a5c97b06f
Александр Маджугин <madzhugin@gmail.com>
2025-11-04 12:42:31 +0000

    добавление релейшенс

---
+++ local/modules/x.relations/install/index.php
@@ -0,0 +1,21 @@
+<?php
+if (!class_exists('X\Relations\Module')) include __DIR__.'/../lib/module.php';
+if (!class_exists('X\Relations\Module')) return;
+class x_relations extends \X\Relations\Module
+// class x_relations extends CModule
+{
+    /*
+    public $MODULE_ID = 'x.relations';
+    
+    function __construct() {
+        $this->PARTNER_NAME = 'x';
+        $this->PARTNER_URI = 'https://x.ru';
+        parent::__construct();
+    }
+    */
+}
+
+/*
+public function DoInstall () {}
+public function DoUninstall () {}
+*/
---
+++ local/modules/x.relations/lang/ru/lib/module.php
@@ -0,0 +1,9 @@
+<?php
+$MESS['X_RELATIONS_INSTALL_NAME'] = 'Связи объектов';
+$MESS['X_RELATIONS_INSTALL_DESCRIPTION'] = 'Модуль добавляеющий ссылки между объектами данных битрикс в административной части';
+$MESS['X_RELATIONS_INSTALL_TITLE'] = 'Установка модуля';
+$MESS['X_RELATIONS_INSTALL_MESSAGE'] = 'Модуль установлен';
+$MESS['X_RELATIONS_UNINSTALL_TITLE'] = 'Удаление модуля';
+$MESS['X_RELATIONS_UNINSTALL_MESSAGE'] = 'Модуль успешно удален из системы';
+$MESS['X_RELATIONS_PARTNER'] = '';
+$MESS['X_RELATIONS_PARTNER_URI'] = '';
